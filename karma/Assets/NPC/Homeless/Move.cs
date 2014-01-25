7fb0e2730ff695ec70415161eccd8a75adc7826b
Jeremy <jeremy.tuloup@gmail.com>
2014-01-25 16:55:31 +0100

    NPC moving around.

--- karma/Assets/NPC/Homeless/Move.cs
+++ karma/Assets/NPC/Homeless/Move.cs
@@ -1,18 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Move : MonoBehaviour {
 
-	public Transform target;
+	public float wayPointDist;
 
-	// Use this for initialization
+	Transform t;
+	Transform target;
+	NavMeshAgent agent;
+	List<Transform> wayPoints;
+
+	void Awake() {
+		t = transform;
+		agent = GetComponent<NavMeshAgent>();
+	}
+
 	void Start () {
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+		if (target != null && Vector3.Distance(target.position, t.position) < wayPointDist) {
+			SetTarget(wayPoints[Random.Range(0, wayPoints.Count-1)]);
+		}
+	}
+
+	public void SetWayPoints(List<Transform> ts) {
+		wayPoints = ts;
+	}
+
+	public void SetTarget(Transform tr) {
+		target = tr;
+		agent.SetDestination(target.position);
 	}
 
 }
--- karma/Assets/NPC/Spawn.cs
+++ karma/Assets/NPC/Spawn.cs
@@ -1,40 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Spawn : MonoBehaviour {
-
-	public int numberWanderingHomeless;
+	
 	public Transform homelessPrefab;
 
-	float width = 50;
-	float height = 50;
-
-	Vector3 raycastPos;
-	float rayCastHeight = 1000.0f;
-
-	LayerMask layerGround, layer;
-	
 	void Start () {
+		Transform[] wayPoints = new Transform[transform.childCount];
+		List<Transform> ts = new List<Transform>();
+		foreach (Transform t in transform) ts.Add(t);
 
-		layerGround = LayerMask.NameToLayer("Ground");
-		layer = (1 << layerGround);
 
-		Vector3 raycastPos = new Vector3(0, rayCastHeight, 0);
-		for (int i = 0; i < numberWanderingHomeless; i++) {
-			RaycastHit hit;
-			do {
-				float x = Random.Range(0, width);
-				float z = Random.Range(0, height);
-				raycastPos = new Vector3(x, rayCastHeight, z);
-				Debug.Log (x + ", " + z);
-			} while (!Physics.Raycast(raycastPos, -Vector3.up, out hit, Mathf.Infinity, layer));
-			Vector3 hitPos = new Vector3(hit.point.x, 0.5f, hit.point.z);
-
-			Instantiate(homelessPrefab, hitPos, Quaternion.identity);
+		foreach(Transform t in ts) {
+			Transform tr = Instantiate(homelessPrefab, t.position, Quaternion.identity) as Transform;
+			tr.GetComponent<Move>().SetWayPoints(ts);
+			tr.GetComponent<Move>().SetTarget(t);
 		}
 	}
 
 	void Update () {
 	
 	}
+
+
 }
