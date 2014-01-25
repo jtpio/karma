1221799b6664f5096519b3f59aa402c5cce2ba8a
Jeremy <jeremy.tuloup@gmail.com>
2014-01-25 21:15:08 +0100

    Intro screen + minor changes

---
+++ karma/Assets/GameStuff/Config.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Config : MonoBehaviour {
+	
+	void Start () {
+		Screen.lockCursor = true;
+	}
+
+	void OnMouseDown() {
+		Screen.lockCursor = true;
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.Escape)) {
+			Screen.lockCursor = false;
+		}
+	}
+}
---
+++ karma/Assets/Intro/Instructions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instructions : MonoBehaviour {
+
+	public GUIStyle style;
+	public string text;
+
+	void Start () {
+	}
+
+	void Update () {
+		if (Input.anyKey) {
+			Application.LoadLevel(1);
+		}
+	}
+
+	void OnGUI () {
+		style.fontSize = 42 + (int) (Mathf.Cos(Time.time * 10) * 20);
+		var dim = style.CalcSize(new GUIContent(text));
+		GUI.Label(new Rect(Screen.width / 2 - dim.x / 2, Screen.height / 2 - dim.y / 2, 200, 200), text, style);
+	}
+}
