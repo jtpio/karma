467fbdcf608e007040116c891259b4ace6deb703
Husenap <Hussein_t_97@hotmail.com>
2014-01-25 22:01:46 +0100

    moneyCounter changes

--- karma/Assets/Player/MoneyCounter.cs
+++ karma/Assets/Player/MoneyCounter.cs
@@ -6,38 +6,50 @@ public class MoneyCounter : MonoBehaviour {
 	public int moneyStart;
 	public int moneyEnd;
 	public int moneyStep;
+	public int decrease;
 	public string retryText;
 	public GUIStyle style;
 	public GUIStyle retryStyle;
 
 	int money;
-	bool lost;
+	enum State{
+		COLLECT, LOSING, LOST
+	}
+	State state;
 
 	void Start () {
 		money = moneyStart;
-		lost = false;
+		state = State.COLLECT;
 	}
 	
 	void Update () {
-		if (Input.GetKey(KeyCode.R)) {
-			Application.LoadLevel(0);
-		}
+		if(money <= 0) state = State.LOST;
 	}
 
 	public void incMoney() {
-		if (!lost) {
+		if (state == State.COLLECT) {
 			money += Random.Range(moneyStep / 2, moneyStep);
-			lost = money > moneyEnd;
+			if(money > moneyEnd) state = State.LOSING;
 		}
 	}
 
 	public void OnGUI() {
-		style.fontSize = (lost) ? 42 + (int) (Mathf.Cos(Time.time * 10) * 10) : 42;
+		style.fontSize = (state == State.LOST) ? 42 + (int) (Mathf.Cos(Time.time * 10) * 10) : 42;
 		GUI.Label(new Rect(5, 0, 150, 100), 
 			"" + money, 
 			style
 		);
 
-		if (lost) GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200), retryText, retryStyle);
+		if(state == State.LOSING){
+			money -= (int)(Time.deltaTime * decrease);
+		}
+
+		if (state == State.LOST) {
+			GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200), retryText, retryStyle);
+			money = 0;
+			if (Input.GetKey(KeyCode.R)) {
+				Application.LoadLevel(0);
+			}
+		}
 	}
 }
