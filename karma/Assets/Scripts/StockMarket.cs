473b0cdb160364fb8f20645d30812fa6b42e22e9
Husenap <Hussein_t_97@hotmail.com>
2014-01-25 14:53:34 +0100

    stock market + arms

---
+++ karma/Assets/Scripts/StockMarket.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StockMarket : MonoBehaviour {
+
+	float newX = 0;
+	public float step;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiTexture.pixelInset = new Rect(0, 0, Mathf.Min(256, (int)newX), 116);
+		newX += Time.deltaTime;
+	}
+}
