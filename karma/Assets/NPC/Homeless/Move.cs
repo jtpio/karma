2b3aacd78b9f1b382ad52c335b79a074365c0718
Jeremy <jeremy.tuloup@gmail.com>
2014-01-25 13:48:52 +0100

    First character + controls + navMesh + city and sidewalks!

---
+++ karma/Assets/NPC/Homeless/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+	}
+
+}
---
+++ karma/Assets/NPC/Spawn.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public int numberWanderingHomeless;
+	public Transform homelessPrefab;
+
+	float width = 50;
+	float height = 50;
+
+	Vector3 raycastPos;
+	float rayCastHeight = 1000.0f;
+
+	LayerMask layerGround, layer;
+	
+	void Start () {
+
+		layerGround = LayerMask.NameToLayer("Ground");
+		layer = (1 << layerGround);
+
+		Vector3 raycastPos = new Vector3(0, rayCastHeight, 0);
+		for (int i = 0; i < numberWanderingHomeless; i++) {
+			RaycastHit hit;
+			do {
+				float x = Random.Range(0, width);
+				float z = Random.Range(0, height);
+				raycastPos = new Vector3(x, rayCastHeight, z);
+				Debug.Log (x + ", " + z);
+			} while (!Physics.Raycast(raycastPos, -Vector3.up, out hit, Mathf.Infinity, layer));
+			Vector3 hitPos = new Vector3(hit.point.x, 0.5f, hit.point.z);
+
+			Instantiate(homelessPrefab, hitPos, Quaternion.identity);
+		}
+	}
+
+	void Update () {
+	
+	}
+}
